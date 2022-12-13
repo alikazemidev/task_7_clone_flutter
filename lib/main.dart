32ef46423f22a04500062f1bd1fefee22b4ff37d
alikz <ali6316k@gmail.com>
2022-12-13 19:56:04 +0330

    create edit task screen

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ const taskBoxName = 'tasks';
 void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(TaskAdapter());
-  Hive.openBox<Task>(taskBoxName);
+  await Hive.openBox<Task>(taskBoxName);
   runApp(const MyApp());
 }
 
@@ -31,12 +31,84 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final box = Hive.box<Task>(taskBoxName);
+    return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) {
+                  return EditTaskScreen();
+                },
+              ),
+            );
+          },
+          label: Row(
+            children: [
+              Text('Add New Task'),
+              Icon(Icons.add),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: Text('Todo List'),
+        ),
+        body: ListView.builder(
+          itemBuilder: (context, index) {
+            return Container(
+              color: Colors.red,
+              height: 200,
+              width: double.infinity,
+              child: Text(box.values.toList()[index].name!),
+            );
+          },
+          itemCount: box.values.length,
+        ));
+  }
+}
+
+class EditTaskScreen extends StatelessWidget {
+  TextEditingController _controller = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Todo List'),
+        title: Text('add task'),
       ),
-      body: Container(
-        color: Colors.red,
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            final task = Task(
+              name: _controller.text,
+              priority: Priority.low,
+              isCompleted: false,
+            );
+            //Todo uncomment this
+            // if (task.isInBox) {
+            //   task.save();
+            // } else {
+            //   final Box<Task> box = Hive.box(taskBoxName);
+            //   box.add(task);
+            // }
+            Navigator.pop(context);
+          },
+          label: Row(
+            children: [
+              Text('save changes'),
+              Icon(Icons.check),
+            ],
+          )),
+      body: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            decoration: InputDecoration(
+              label: Text('Add a task for today...'),
+            ),
+          ),
+        ],
       ),
     );
   }
